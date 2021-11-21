1de1461423e8b8f1dad918d8c612cd254a4bac62
10 <609438328@qq.com>
2021-11-21 16:37:25 +0800

    animation

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,6 +1,6 @@
 
 <script setup>
-import { ref,onMounted } from "vue";
+import { ref,onMounted,nextTick } from "vue";
 import  axios  from 'axios';
 import { ElMessage } from 'element-plus'
 onMounted(() => {
@@ -16,67 +16,141 @@ var curPage = 1
 var str = null;
 var btnDisable = ref(false)
 var isLoading = ref(false)
+var renderComponent = ref(true);
+var A1 = ref();
+var A2 = ref();
+var A3 = ref();
+var A4 = ref();
+var B1 = ref();
+var B2 = ref();
+var B3 = ref();
+var B4 = ref();
+var C = ref();
+var D = ref();
+let MAP = {
+        'A1' : A1,
+        'A2' : A2,
+        'A3' : A3,
+        'A4' : A4,
+        'B1' : B1,
+        'B2' : B2,
+        'B3' : B3,
+        'B4' : B4,
+        'C' : C,
+        'D' : D
+    }
+function forceRerender() {
+  renderComponent = false;
+
+    return new Promise((resolve)=> {
+        nextTick().then(() => {
+            renderComponent = true;
+            resolve();
+        });
+    })
+  // 从 DOM 中删除 my-component 组件
+
+  
+}
+function animate(element,direction,target){
+        clearInterval(element.timer1);
+        element.timer1=setInterval(function(){//element是一个对象，对象点出来的属性有且只有一个，避免多次点击按钮产生多个定时器
+            var current=parseInt(element.style[direction]);    //获取当前位置，数字类型，没有px。
+            //不可以用element.style.left，因为该写法只能获取到行内样式，不能获取到<style></style>标签内的样式
+            var step=5;//每次移动的距离
+            step=current<target?step:-step;//step的正负表示了向左或是向右移动
+            current+=step;    //计算移动到的位置，数字类型，没有px                
+            if(Math.abs(target-current)>Math.abs(step)){//当离目标位置的距离大于一步移动的距离
+                element.style[direction]=current+"px";//移动
+            
+            }else{//当间距小于一步的距离，则清理定时器，把元素直接拿到目标位置
+                clearInterval(element.timer1);
+                element.style[direction]=target+"px";
+                
+            }
+        },10);
+    }
 async function play(){
     console.log('play')
     btnDisable.value = true;
     let playIndex = curPage;
-    await initPosition()
+    initPosition()
     var stepList = []
     if(posObj.step && posObj.step != '[]'){
         stepList = (posObj.step).replace('[(','').replace(')]','').split('), (')
     }
+    var chess;
     for(let i=0;i<stepList.length;i++){
         if(playIndex != curPage){
             console.log('returns')
             stepCount.value = 0;
+            Velocity(chess,"finish")
             return;
         }
         let temp = stepList[i].split(', ')
-        let chess = document.getElementsByClassName(CHESS_LIST[temp[0]])[0];
+        chess = MAP[CHESS_LIST[temp[0]]];
+        console.log(chess.top,chess.left)
+        chess.value.style.top = chess.top + 'px'
+        chess.value.style.left = chess.left + 'px'
         let action = temp[1];
         let speed = '100px';
         if(action == '0'){//up
-            // chess.style.top = (parseInt(chess.style.top) - 100) + 'px'
-            let target = (parseInt(chess.style.top) - 100)
-            console.log((parseInt(chess.style.top) - 100) + 'px')
-            Velocity(chess,{
-                top : target + 'px'
-            },{
-                duration:300
-            })
-            await sleep(400);
-            chess.style.top = target + 'px'
+            let target = (chess.top - 100)
+            animate(chess.value,'top',target)
+            // chess.style.top  = parseInt(chess.style.top) + 'px'
+            // console.log((parseInt(chess.style.top) - 100) + 'px')
+            // Velocity(chess.value,{
+            //     top : target + 'px'
+            // },{
+            //     duration:300
+            // })
+            await sleep(300);
+            chess.top = target
+            console.log(chess)
+
         }else if(action == '1'){//right
             // chess.style.left = (parseInt(chess.style.left) + 100) + 'px'
-            let target = (parseInt(chess.style.left) + 100)
-            console.log(target + 'px')
-            Velocity(chess,{
-                left : target + 'px'
-            },{
-                duration:300
-            })
-            await sleep(400);
-            chess.style.left = target + 'px'
+            let target  = (chess.left+100)
+            animate(chess.value,'left',target)
+
+            // let target = (parseInt(chess.style.left) + 100)
+            // console.log(target + 'px')
+            // Velocity(chess.value,{
+            //     left : target + 'px'
+            // },{
+            //     duration:300
+            // })
+            await sleep(300);
+            chess.left = target
+            console.log(chess)
+
         }else if(action == '2'){//down
-            let target = (parseInt(chess.style.top) + 100)
-            console.log(target + 'px')
-            Velocity(chess,{
-                top : target + 'px'
-            },{
-                duration:300
-            })
-            await sleep(400);
-            chess.style.top = target + 'px'
+            // chess.style.top  = parseInt(chess.style.top) + 'px'
+            let target = (chess.top + 100)
+            animate(chess.value,'top',target)
+            // console.log(target + 'px')
+            // Velocity(chess.value,{
+            //     top : target + 'px'
+            // },{
+            //     duration:300
+            // })
+            await sleep(300);
+            chess.top = target
+            console.log(chess)
         }else if(action == '3'){//left
-            let target = (parseInt(chess.style.left) - 100)
-            console.log(target + 'px')
-            Velocity(chess,{
-                left : target + 'px'
-            },{
-                duration:300,
-            })
-            await sleep(400);
-            chess.style.left = target + 'px'
+            // chess.style.left  = parseInt(chess.style.left) + 'px'
+            let target = (chess.left - 100)
+            animate(chess.value,'left',target)
+            // console.log(target + 'px')
+            // Velocity(chess.value,{
+            //     left : target + 'px'
+            // },{
+            //     duration:300,
+            // })
+            await sleep(300);
+            // chess.style.left = target + 'px'
+            chess.left = target
+            console.log(chess)
         }
         // console.log(Velocity,22)
 
@@ -126,38 +200,23 @@ function getPosition(){
         isLoading.value = false;
     })
 }
-async function initPosition(){
-    let A1 = document.getElementsByClassName('A1')[0]
-    let A2 = document.getElementsByClassName('A2')[0]
-    let A3 = document.getElementsByClassName('A3')[0]
-    let A4 = document.getElementsByClassName('A4')[0]
-    let B1 = document.getElementsByClassName('B1')[0]
-    let B2 = document.getElementsByClassName('B2')[0]
-    let B3 = document.getElementsByClassName('B3')[0]
-    let B4 = document.getElementsByClassName('B4')[0]
-    let C = document.getElementsByClassName('C')[0]
-    let D = document.getElementsByClassName('D')[0]
-    let MAP = {
-        'A1' : A1,
-        'A2' : A2,
-        'A3' : A3,
-        'A4' : A4,
-        'B1' : B1,
-        'B2' : B2,
-        'B3' : B3,
-        'B4' : B4,
-        'C' : C,
-        'D' : D
-    }
-    await sleep(400);
-    console.log('posObj',posObj)
+function initPosition(){
+    isLoading.value = true;
     for(let i=0;i<posObj.pos.length;i++){
         let x = (posObj.pos[i]%4)*100;
         let y= (Math.floor(posObj.pos[i]/4))*100;
         // MAP[CHESS_LIST[i]].style.top = '400px'
-        MAP[CHESS_LIST[i]].style.left = x + 'px'
-        MAP[CHESS_LIST[i]].style.top = y + 'px'
+        if(MAP[CHESS_LIST[i]].value.timer1){
+            clearInterval(MAP[CHESS_LIST[i]].value.timer1)
+        }
+        MAP[CHESS_LIST[i]].left = x
+        MAP[CHESS_LIST[i]].top = y
+        MAP[CHESS_LIST[i]].value.style.left = x + 'px'
+        MAP[CHESS_LIST[i]].value.style.top = y + 'px'
     }
+    isLoading.value = false;
+
+
 }
 
 // var newFile = new Blob('klotski.bin');
@@ -165,26 +224,26 @@ async function initPosition(){
 //   console.log(newFile.type);
 </script>
 <template>
-    <div class="content" v-loading="isLoading">
+    <div class="content" v-loading="isLoading" v-if="renderComponent">
         <div class="header">
             <span>KLOTSKI</span>
         </div>
-        <div class="main">
-            <div class="item A A1">A1</div>
-            <div class="item A A2">A2</div>
-            <div class="item A A3">A3</div>
-            <div class="item A A4">A4</div>
-            <div class="item B B1">B1</div>
-            <div class="item B B2">B2</div>
-            <div class="item B B3">B3</div>
-            <div class="item B B4">B4</div>
-            <div class="item C">C</div>
-            <div class="item D">D</div>
-        </div>
+        <transition-group name="cell" tag="div" class="container main">
+            <div class="item A A1" ref="A1" key="A1">A1</div>
+            <div class="item A A2" ref="A2" key="A2">A2</div>
+            <div class="item A A3" ref="A3" key="A3">A3</div>
+            <div class="item A A4" ref="A4" key="A4">A4</div>
+            <div class="item B B1" ref="B1" key="B1">B1</div>
+            <div class="item B B2" ref="B2" key="B2">B2</div>
+            <div class="item B B3" ref="B3" key="B3">B3</div>
+            <div class="item B B4" ref="B4" key="B4">B4</div>
+            <div class="item C" ref="C" key="C">C</div>
+            <div class="item D" ref="D" key="D">D</div>
+        </transition-group>
         <div class="pagination">
             <div class="f1">
                 <el-button size="mini" type="primary" :disabled="btnDisable" class="play" @click="play">play</el-button>
-                <div class="count">COUNT：{{stepCount}}</div>
+                <div class="count">TIME:0 COUNT：{{stepCount}}</div>
             </div> 
             <el-pagination
                 class="pag"
@@ -235,7 +294,7 @@ async function initPosition(){
 .count{
     font-size:12px;
     display:inline-box;
-    width:100px;
+    width:200px;
 }
 .play{
 }
@@ -248,6 +307,15 @@ async function initPosition(){
     justify-content:center;
     display:border-box;
 }
+.cell-enter,.cell-leave-to{
+            opacity:  0;/*透明度*/
+            transform: translateX(150px);
+        }
+         /*入场(离场)动画的时间段   */
+        .cell-enter-active,.cell-leave-active{
+            transition: all 0.8s ease;
+
+        }
 .A{
     width:100px;
     height:200px;
