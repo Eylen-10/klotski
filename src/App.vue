f6be01bec5866e467af8cf86e38eeed79245cb84
cyl <609438328@qq.com>
2022-04-15 11:25:25 +0800

    2d

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 // import Klotski from './components/home2.vue'
-import Klotski from './components/home2.vue'
+import Klotski from './components/home.vue'
 </script>
 
 <template>
